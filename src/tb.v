658a774a9533b5afdae6972cfd07518d9e25e4ce
Karina <aguilar7821@gmail.com>
2023-11-02 01:26:59 -0700

    forgor to name the instance calling top module

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_topModuleKA (
+    tt_um_topModuleKA tt_um_topModuleKA(
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
